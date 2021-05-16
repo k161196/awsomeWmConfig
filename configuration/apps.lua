ec07025e58ed0b14f054f5e8d70a86de702aee0b
k161196 <kiranyadav161196@gmail.com>
2021-05-16 20:36:07 +0530

    push

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,7 +8,8 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'terminator',
+    -- terminal = 'terminator',
+    terminal = 'kitty',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
     quake = 'terminator',
